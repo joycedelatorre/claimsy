3f32e878a063208d5d5c335ea1a7e8450669d47a
joyce <joycedelatorre87@gmail.com>
2018-01-05 11:07:54 -0600

    PASS: create new transaction and insert to DB

--- public/js/transaction.js
+++ public/js/transaction.js
@@ -1,11 +1,6 @@
-$(document).ready(function(){
+var userId = GetQueryStringParams("userId");
 
-
-//--------------------- READ TRANSACTION DATA
-  $("#listTransac").on("click", function(event){ //to get all the transaction data from DB
-    event.preventDefault();
-    //console.log("testing");
-    var userId = GetQueryStringParams("userId");
+function getTransactions(){
     $.get("/api/transactions/"+userId, function(data){
       //console.log(data[0]);
       var rowsToAdd =[];
@@ -14,19 +9,40 @@ $(document).ready(function(){
         //console.log(data[i]);
       }
       renderTransacList(rowsToAdd);
-    })
-  });
-
-  function renderTransacList(rows){ //this will render all the transactions of a user
+    });
+  }
+ function renderTransacList(rows){ //this will render all the transactions of a user
     //console.log(rows[0]);
+    $( ".lTransac" ).remove();
     for(var i=0; i<rows.length; i++){
-      $(".table-striped").append("<tr><td>" + rows[i].provider +
+      $(".table-striped").append("<tr class='lTransac'><td>" + rows[i].provider +
        "</td><td>"+ rows[i].description + "</td><td>"+ rows[i].amount+
-       "</td><td>"+ rows[i].status +"</td><td>"+ rows[i].createdAt+"</td></tr>"
+       "</td><td>"+ rows[i].status +"</td><td>"+ rows[i].createdAt+
+       "</td><td>"+"<button class='btn btn-success'> Edit </button>"+
+       "</td><td>"+"<button class='btn btn-success'>Delete</button>"+
+       "</td></tr>"
       );
     }
   }
+$(document).ready(function(){
 
+
+//--------------------- READ TRANSACTION DATA
+  $("#listTransac").on("click", function(event){ //to get all the transaction data from DB
+    event.preventDefault();
+    //console.log("testing");
+    // var userId = GetQueryStringParams("userId");
+    getTransactions();
+    // $.get("/api/transactions/"+userId, function(data){
+    //   //console.log(data[0]);
+    //   var rowsToAdd =[];
+    //   for(var i =0; i < data.length; i++){
+    //     rowsToAdd.push(data[i]);
+    //     //console.log(data[i]);
+    //   }
+    //   renderTransacList(rowsToAdd);
+    // })
+  });
 });
 
 function GetQueryStringParams(sParam){ // this is to read the url for userId
@@ -41,5 +57,29 @@ function GetQueryStringParams(sParam){ // this is to read the url for userId
       }
     }
 }
+
+//--------------------------- CREATE TRANSACTION DATA
+
+$("#add").on("click", function(event){
+  event.preventDefault();
+  insertTransaction(
+    {
+      provider:$("#providerInput").val().trim(),
+      description:$("#descriptionInput").val().trim(),
+      amount:$("#amountInput").val().trim(),
+      status:$("#statusInput").val().trim(),
+      UserId:userId
+    }
+  );
+});
+
+function insertTransaction(transactionData){
+  $.post("/api/new", transactionData)
+    .then(getTransactions);
+}
+
+//--------------------------- UPDATE TRANSACTION DATA
+
+//--------------------------- DELETE TRANSACTION DATA
 
 
--- routes/transaction-api-routes.js
+++ routes/transaction-api-routes.js
@@ -2,13 +2,19 @@ var db = require("../models");
 
 module.exports = function(app){
 
-  app.get("/api/transactions/:id", function(req, res){
+  app.get("/api/transactions/:id", function(req, res){ //list all transaction
     var userId = req.params.id;
     db.Transaction.findAll({
       where:{
         UserId: userId
       }
     }).then(function(dbTransaction){
+      res.json(dbTransaction);
+    });
+  });
+
+  app.post("/api/new", function(req, res) { //create new transaction
+    db.Transaction.create(req.body).then(function(dbTransaction) { 
       res.json(dbTransaction);
     });
   });
